0ca3b596100d4c6bdd5783c8cdbf14850f6b7814
Rafael Silva Alves <rafa266528@gmail.com>
2018-05-22 13:52:49 -0300

    Initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Runtime;
+using System;
+
+namespace TesteJapaMiiller
+{
+    [Activity(Label = "TesteJapaMiiller", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button entrar;
+        TextView textView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            entrar = FindViewById<Button>(TesteJapaMiiller.Resource.Id.entrar);
+            textView = FindViewById<TextView>(TesteJapaMiiller.Resource.Id.textView1);
+            entrar.Click += entrar_Click;
+        }
+
+        void entrar_Click(object sender, EventArgs e)
+        {
+            textView.Text = "teste";
+        }
+    }
+}
+
